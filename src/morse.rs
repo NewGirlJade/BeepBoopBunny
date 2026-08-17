254481c640382043052a3e88e4b3d3dfd3f87df3
Jayda <JadelynneMiriam@gmail.com>
2026-08-17 01:34:33 -0600

    first draft morse to string function, tests to come soon

--- src/morse.rs
+++ src/morse.rs
@@ -1,4 +1,7 @@
 use agb::hash_map::HashMap;
+use alloc::borrow::ToOwned;
+use alloc::string::String;
+use alloc::vec;
 use alloc::vec::Vec;
 use core::cell::LazyCell;
 use core::ops::Deref;
@@ -8,59 +11,118 @@ static MORSE_BINDINGS: SuperLazyCell<HashMap<char, Vec<MorseSegment>>> = SuperLa
     let dot = MorseSegment::Dot;
     let dash = MorseSegment::Dash;
     let mut map = HashMap::new();
-    map.insert('a', alloc::vec![dot, dash]);
-    map.insert('b', alloc::vec![dash, dot, dot, dot]);
-    map.insert('c', alloc::vec![dash, dot, dash, dot]);
-    map.insert('d', alloc::vec![dash, dot, dot]);
-    map.insert('e', alloc::vec![dot]);
-    map.insert('f', alloc::vec![dot, dot, dash, dot]);
-    map.insert('g', alloc::vec![dash, dash, dot]);
-    map.insert('h', alloc::vec![dot, dot, dot, dot]);
-    map.insert('i', alloc::vec![dot, dot]);
-    map.insert('j', alloc::vec![dot, dash, dash, dash]);
-    map.insert('k', alloc::vec![dash, dot, dash]);
-    map.insert('l', alloc::vec![dot, dash, dot, dot]);
-    map.insert('m', alloc::vec![dash, dash]);
-    map.insert('n', alloc::vec![dash, dot]);
-    map.insert('o', alloc::vec![dash, dash, dash]);
-    map.insert('p', alloc::vec![dot, dash, dash, dot]);
-    map.insert('q', alloc::vec![dash, dash, dot, dash]);
-    map.insert('r', alloc::vec![dot, dash, dot]);
-    map.insert('s', alloc::vec![dot, dot, dot]);
-    map.insert('t', alloc::vec![dash]);
-    map.insert('u', alloc::vec![dot, dot, dash]);
-    map.insert('v', alloc::vec![dot, dot, dot, dash]);
-    map.insert('w', alloc::vec![dot, dash, dash]);
-    map.insert('x', alloc::vec![dash, dot, dot, dash]);
-    map.insert('y', alloc::vec![dash, dot, dash, dash]);
-    map.insert('z', alloc::vec![dash, dash, dot, dot]);
-    map.insert(' ', alloc::vec![MorseSegment::WordEnd]);
-    map.insert('0', alloc::vec![dash, dash, dash, dash, dash]);
-    map.insert('1', alloc::vec![dot, dash, dash, dash, dash]);
-    map.insert('2', alloc::vec![dot, dot, dash, dash, dash]);
-    map.insert('3', alloc::vec![dot, dot, dot, dash, dash]);
-    map.insert('4', alloc::vec![dot, dot, dot, dot, dash]);
-    map.insert('5', alloc::vec![dot, dot, dot, dot, dot]);
-    map.insert('6', alloc::vec![dash, dot, dot, dot, dot]);
-    map.insert('7', alloc::vec![dash, dash, dot, dot, dot]);
-    map.insert('8', alloc::vec![dash, dash, dash, dot, dot]);
-    map.insert('9', alloc::vec![dash, dash, dash, dash, dot]);
-    map.insert('.', alloc::vec![dot, dash, dot, dash, dot, dash]);
-    map.insert('?', alloc::vec![dot, dot, dash, dash, dot, dot]);
-    map.insert(',', alloc::vec![dash, dash, dot, dot, dash, dash]);
-    map.insert('\'', alloc::vec![dot, dash, dash, dash, dash, dot]); // ' char
-    map.insert('"', alloc::vec![dot, dash, dot, dot, dash, dot]);
-    map.insert(':', alloc::vec![dash, dash, dash, dot, dot, dot]);
-    map.insert('+', alloc::vec![dot, dash, dot, dash, dot]);
-    map.insert('=', alloc::vec![dash, dot, dot, dot, dash]);
-    map.insert('/', alloc::vec![dash, dot, dot, dash, dot]);
-    map.insert('-', alloc::vec![dash, dot, dot, dot, dot, dash]);
-    map.insert('(', alloc::vec![dash, dot, dash, dash, dot]);
-    map.insert(')', alloc::vec![dash, dot, dash, dash, dot, dash]);
-    map.insert('&', alloc::vec![dot, dash, dot, dot, dot]);
-    map.insert('@', alloc::vec![dot, dash, dash, dot, dash, dot]);
+    map.insert('a', vec![dot, dash]);
+    map.insert('b', vec![dash, dot, dot, dot]);
+    map.insert('c', vec![dash, dot, dash, dot]);
+    map.insert('d', vec![dash, dot, dot]);
+    map.insert('e', vec![dot]);
+    map.insert('f', vec![dot, dot, dash, dot]);
+    map.insert('g', vec![dash, dash, dot]);
+    map.insert('h', vec![dot, dot, dot, dot]);
+    map.insert('i', vec![dot, dot]);
+    map.insert('j', vec![dot, dash, dash, dash]);
+    map.insert('k', vec![dash, dot, dash]);
+    map.insert('l', vec![dot, dash, dot, dot]);
+    map.insert('m', vec![dash, dash]);
+    map.insert('n', vec![dash, dot]);
+    map.insert('o', vec![dash, dash, dash]);
+    map.insert('p', vec![dot, dash, dash, dot]);
+    map.insert('q', vec![dash, dash, dot, dash]);
+    map.insert('r', vec![dot, dash, dot]);
+    map.insert('s', vec![dot, dot, dot]);
+    map.insert('t', vec![dash]);
+    map.insert('u', vec![dot, dot, dash]);
+    map.insert('v', vec![dot, dot, dot, dash]);
+    map.insert('w', vec![dot, dash, dash]);
+    map.insert('x', vec![dash, dot, dot, dash]);
+    map.insert('y', vec![dash, dot, dash, dash]);
+    map.insert('z', vec![dash, dash, dot, dot]);
+    map.insert(' ', vec![MorseSegment::WordEnd]);
+    map.insert('0', vec![dash, dash, dash, dash, dash]);
+    map.insert('1', vec![dot, dash, dash, dash, dash]);
+    map.insert('2', vec![dot, dot, dash, dash, dash]);
+    map.insert('3', vec![dot, dot, dot, dash, dash]);
+    map.insert('4', vec![dot, dot, dot, dot, dash]);
+    map.insert('5', vec![dot, dot, dot, dot, dot]);
+    map.insert('6', vec![dash, dot, dot, dot, dot]);
+    map.insert('7', vec![dash, dash, dot, dot, dot]);
+    map.insert('8', vec![dash, dash, dash, dot, dot]);
+    map.insert('9', vec![dash, dash, dash, dash, dot]);
+    map.insert('.', vec![dot, dash, dot, dash, dot, dash]);
+    map.insert('?', vec![dot, dot, dash, dash, dot, dot]);
+    map.insert(',', vec![dash, dash, dot, dot, dash, dash]);
+    map.insert('\'', vec![dot, dash, dash, dash, dash, dot]); // ' char
+    map.insert('"', vec![dot, dash, dot, dot, dash, dot]);
+    map.insert(':', vec![dash, dash, dash, dot, dot, dot]);
+    map.insert('+', vec![dot, dash, dot, dash, dot]);
+    map.insert('=', vec![dash, dot, dot, dot, dash]);
+    map.insert('/', vec![dash, dot, dot, dash, dot]);
+    map.insert('-', vec![dash, dot, dot, dot, dot, dash]);
+    map.insert('(', vec![dash, dot, dash, dash, dot]);
+    map.insert(')', vec![dash, dot, dash, dash, dot, dash]);
+    map.insert('&', vec![dot, dash, dot, dot, dot]);
+    map.insert('@', vec![dot, dash, dash, dot, dash, dot]);
     map
 });
+
+static REVERSE_MORSE_BINDINGS: SuperLazyCell<HashMap<Vec<MorseSegment>, char>> =
+    SuperLazyCell::new(|| {
+        let dot = MorseSegment::Dot;
+        let dash = MorseSegment::Dash;
+        let mut map = HashMap::new();
+        map.insert(vec![dot, dash], 'a');
+        map.insert(vec![dash, dot, dot, dot], 'b');
+        map.insert(vec![dash, dot, dash, dot], 'c');
+        map.insert(vec![dash, dot, dot], 'd');
+        map.insert(vec![dot], 'e');
+        map.insert(vec![dot, dot, dash, dot], 'f');
+        map.insert(vec![dash, dash, dot], 'g');
+        map.insert(vec![dot, dot, dot, dot], 'h');
+        map.insert(vec![dot, dot], 'i');
+        map.insert(vec![dot, dash, dash, dash], 'j');
+        map.insert(vec![dash, dot, dash], 'k');
+        map.insert(vec![dot, dash, dot, dot], 'l');
+        map.insert(vec![dash, dash], 'm');
+        map.insert(vec![dash, dot], 'n');
+        map.insert(vec![dash, dash, dash], 'o');
+        map.insert(vec![dot, dash, dash, dot], 'p');
+        map.insert(vec![dash, dash, dot, dash], 'q');
+        map.insert(vec![dot, dash, dot], 'r');
+        map.insert(vec![dot, dot, dot], 's');
+        map.insert(vec![dash], 't');
+        map.insert(vec![dot, dot, dash], 'u');
+        map.insert(vec![dot, dot, dot, dash], 'v');
+        map.insert(vec![dot, dash, dash], 'w');
+        map.insert(vec![dash, dot, dot, dash], 'x');
+        map.insert(vec![dash, dot, dash, dash], 'y');
+        map.insert(vec![dash, dash, dot, dot], 'z');
+        map.insert(vec![MorseSegment::WordEnd], ' ');
+        map.insert(vec![dash, dash, dash, dash, dash], '0');
+        map.insert(vec![dot, dash, dash, dash, dash], '1');
+        map.insert(vec![dot, dot, dash, dash, dash], '2');
+        map.insert(vec![dot, dot, dot, dash, dash], '3');
+        map.insert(vec![dot, dot, dot, dot, dash], '4');
+        map.insert(vec![dot, dot, dot, dot, dot], '5');
+        map.insert(vec![dash, dot, dot, dot, dot], '6');
+        map.insert(vec![dash, dash, dot, dot, dot], '7');
+        map.insert(vec![dash, dash, dash, dot, dot], '8');
+        map.insert(vec![dash, dash, dash, dash, dot], '9');
+        map.insert(vec![dot, dash, dot, dash, dot, dash], '.');
+        map.insert(vec![dot, dot, dash, dash, dot, dot], '?');
+        map.insert(vec![dash, dash, dot, dot, dash, dash], ',');
+        map.insert(vec![dot, dash, dash, dash, dash, dot], '\'');
+        map.insert(vec![dot, dash, dot, dot, dash, dot], '"');
+        map.insert(vec![dash, dash, dash, dot, dot, dot], ':');
+        map.insert(vec![dot, dash, dot, dash, dot], '+');
+        map.insert(vec![dash, dot, dot, dot, dash], '=');
+        map.insert(vec![dash, dot, dot, dash, dot], '/');
+        map.insert(vec![dash, dot, dot, dot, dot, dash], '-');
+        map.insert(vec![dash, dot, dash, dash, dot], '(');
+        map.insert(vec![dash, dot, dash, dash, dot, dash], ')');
+        map.insert(vec![dot, dash, dot, dot, dot], '&');
+        map.insert(vec![dot, dash, dash, dot, dash, dot], '@');
+        map
+    });
 
 // SuperLazyCell and related functions are just so the compiler will stop yelling about LazyCell not being threadsafe. The GBA is single threaded- it's fine.
 struct SuperLazyCell<T, F = fn() -> T>(LazyCell<T, F>);
@@ -85,8 +147,7 @@ const WORDEND: u8 = 3;
 
 //enum to represent all the data one needs to represent morse code compactly
 #[repr(u8)]
-#[cfg_attr(test, derive(Debug))]
-#[derive(Copy, Clone, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 enum MorseSegment {
     Dot = DOT,
     Dash = DASH,
@@ -118,7 +179,7 @@ fn u8_to_morse_segment(variable: u8) -> MorseSegment {
         LETTEREND => MorseSegment::LetterEnd,
         WORDEND => MorseSegment::WordEnd,
         _ => {
-            agb::println!("error: variable {:?} is invalid", variable);
+            agb::println!("error: variable {:?} is invalid.", variable);
             unreachable!();
         }
     }
@@ -131,6 +192,38 @@ fn pack_morse_string(segments: &Vec<MorseSegment>) -> MorseString {
         new_morse_string.pack_segment(*item);
     }
     new_morse_string
+}
+
+fn to_string(segments: &Vec<MorseSegment>) -> String {
+    let mut new_string = String::new();
+    let mut char_segments: Vec<MorseSegment> = Vec::new();
+    for segment in segments {
+        match segment {
+            MorseSegment::Dot | MorseSegment::Dash => char_segments.push(*segment),
+            MorseSegment::LetterEnd | MorseSegment::WordEnd => {
+                flush_char_segments_to_string(&mut char_segments, &mut new_string);
+            }
+        }
+        if *segment == MorseSegment::WordEnd {
+            new_string.push(' ');
+        }
+    }
+    new_string
+}
+
+fn flush_char_segments_to_string(char_segments: &mut Vec<MorseSegment>, new_string: &mut String) {
+    if char_segments.is_empty() {
+        return;
+    };
+    if let Some(found_char) = REVERSE_MORSE_BINDINGS.get(char_segments) {
+        new_string.push(*found_char);
+    } else {
+        agb::println!(
+            "\nNonfatal error: {:?} is not a valid morse sequence. Skipping.\n",
+            *char_segments
+        );
+    };
+    *char_segments = Vec::new();
 }
 
 #[cfg_attr(test, derive(Debug))]
@@ -182,9 +275,9 @@ impl MorseString {
         }
         segments
     }
-    fn to_text(&self) -> Vec<char> {
-        Vec::new()
-        //TODO
+    fn stringify(&self) -> String {
+        let segments = self.unpack();
+        to_string(&segments)
     }
 }
 
@@ -207,12 +300,13 @@ fn string_to_morse(input: alloc::string::String) -> MorseString {
     pack_morse_string(&temp)
 }
 
+///////////////////////////////////////////////////////////////
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     use agb::println;
     use alloc::borrow::ToOwned;
-
-    use super::*;
 
     struct TestContext {
         cluster: MorseCluster,
@@ -228,14 +322,14 @@ mod tests {
             let wend: MorseSegment = MorseSegment::WordEnd;
             TestContext {
                 cluster: MorseCluster(0b00011011),
-                segment_vec: alloc::vec![
+                segment_vec: vec![
                     dot, dot, lend, dot, dash, dash, lend, dash, dot, dash, dot, lend, wend,
                 ],
-                morse_string: pack_morse_string(&alloc::vec![
+                morse_string: pack_morse_string(&vec![
                     dot, dash, lend, dot, lend, wend, dash, dash, dash, dash, dash, lend, dot,
-                    lend, dash, dot, dash, lend, wend
+                    lend, dash, dot, dash, lend, wend,
                 ]),
-                wife_vec: alloc::vec![
+                wife_vec: vec![
                     dot, dot, dot, dot, lend, dot, dot, lend, wend, dash, dot, dash, lend, dot,
                     dash, lend, dash, dot, dash, dash, lend, dot, dash, dot, dot, lend, dot, dash,
                     lend, wend, dash, dash, dash, dot, dot, dot, lend, dot, dot, dot, dash, dash,
@@ -260,7 +354,7 @@ mod tests {
         let Some(result) = MORSE_BINDINGS.get(&'a') else {
             panic!("No bindings found");
         };
-        let expected_result = &alloc::vec![MorseSegment::Dot, MorseSegment::Dash];
+        let expected_result = &vec![MorseSegment::Dot, MorseSegment::Dash];
         assert_eq!(result, expected_result);
     }
     #[test_case]
@@ -305,7 +399,16 @@ mod tests {
         println!("\n{:?}\n", printable);
         assert_eq!(printable, ctx.wife_vec);
     }
+
+    #[test_case]
+    fn test_empty_ms_pack(_gba: &mut agb::Gba) {
+        let empty = pack_morse_string(&Vec::new());
+        let also_empty = MorseString::new();
+        assert_eq!(empty.len, also_empty.len);
+        assert_eq!(empty.data, also_empty.data);
+    }
 }
+
 /*
 ~~~test template~~~
 #[test_case]
